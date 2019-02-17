9b2aac9bcc87298cbcf827497c442483741193dd
Slava Pleshkov <mail@slavapleshkov.com>
2019-02-17 21:56:25 +0200

    edit database seeds

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -2,25 +2,27 @@
 
 use Illuminate\Database\Seeder;
 use App\Comment;
+use Faker\Factory;
 
 class CommentsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        for ($i = 0; $i < 5; $i++) {
-            for ($j = 1; $j < 31; $j++) {
-                $review1 = new Comment();
-                $review1->user_id = 1;
-                $review1->article_id = $j;
-                $review1->text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae orci pellentesque, egestas dui nec, ultricies velit. Donec rutrum quis risus eget eleifend. In sed risus lacus. Integer at posuere nunc, quis lobortis enim.';
-                $review1->status = 1;
-                $review1->save();
-            }
-        }
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$faker = Factory::create();
+		for ($i = 0; $i < 5; $i++) {
+			for ($j = 1; $j < 601; $j++) {
+				$review = new Comment();
+				$review->user_id = 1;
+				$review->article_id = $j;
+				$review->text = $faker->text(150);
+				$review->status = 1;
+				$review->save();
+			}
+		}
+	}
 }
--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -2,20 +2,22 @@
 
 use Illuminate\Database\Seeder;
 use App\Setting;
+use Faker\Factory;
 
 class SettingsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $setting = new Setting();
-        $setting->paginate = 15;
-        $setting->contactform = 1;
-        $setting->footer_about = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae orci pellentesque, egestas dui nec, ultricies velit. Donec rutrum quis risus eget eleifend. In sed risus lacus. Integer at posuere nunc, quis lobortis enim.';
-        $setting->save();
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$faker = Factory::create();
+		$setting = new Setting();
+		$setting->paginate = 15;
+		$setting->contactform = 1;
+		$setting->footer_about = $faker->text(150);
+		$setting->save();
+	}
 }
