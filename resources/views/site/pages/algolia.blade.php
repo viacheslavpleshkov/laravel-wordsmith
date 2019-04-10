1eb33061551f3cb2a5aaaf80a8918cece447bb2a
Slava Pleshkov <mail@slavapleshkov.com>
2019-04-10 22:15:14 +0300

    Fix algolia

--- resources/views/site/pages/algolia.blade.php
+++ resources/views/site/pages/algolia.blade.php
@@ -1,9 +1,8 @@
 @extends('site.layouts.main')
 
 @section('title', "Search")
-
-<section class="s-content s-content--top-padding s-content--narrow">
-	<search-component></search-component>
-</section>
 @section('content')
+	<section class="s-content s-content--top-padding s-content--narrow">
+		<search-component></search-component>
+	</section>
 @endsection
