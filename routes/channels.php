14d2d9e15e3aaea9f4247e22c110de620c07fd46
Slava Pleshkov <mail@slavapleshkov.com>
2019-05-07 21:02:39 +0300

    Add pusher, laravel-echo

--- routes/channels.php
+++ routes/channels.php
@@ -11,6 +11,12 @@
 |
 */
 
+use Illuminate\Support\Facades\Broadcast;
+
 Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int)$user->id === (int)$id;
+});
+
+Broadcast::channel('comment-channel', function () {
+    return true;
 });
