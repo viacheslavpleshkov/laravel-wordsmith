59b8164738d61b75ea43bd2aceddb1b2c1255eb2
Slava Pleshkov <mail@slavapleshkov.com>
2019-10-24 18:41:30 +0300

    Add laradock

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,11 +18,11 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-		\App\Http\Middleware\CheckForMaintenanceMode::class,
-		\Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-		\App\Http\Middleware\TrimStrings::class,
-		\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-		\App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     /**
@@ -32,13 +32,13 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-			\App\Http\Middleware\EncryptCookies::class,
-			\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-			\Illuminate\Session\Middleware\StartSession::class,
-			 \Illuminate\Session\Middleware\AuthenticateSession::class,
-			\Illuminate\View\Middleware\ShareErrorsFromSession::class,
-			\App\Http\Middleware\VerifyCsrfToken::class,
-			\Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+             \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
@@ -55,15 +55,16 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-		'auth' => \App\Http\Middleware\Authenticate::class,
-		'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-		'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-		'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-		'can' => \Illuminate\Auth\Middleware\Authorize::class,
-		'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-		'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-		'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-		'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'roles' => \App\Http\Middleware\CheckRole::class,
     ];
 
@@ -75,11 +76,12 @@ class Kernel extends HttpKernel
 	 * @var array
 	 */
 	protected $middlewarePriority = [
-		\Illuminate\Session\Middleware\StartSession::class,
-		\Illuminate\View\Middleware\ShareErrorsFromSession::class,
-		\App\Http\Middleware\Authenticate::class,
-		\Illuminate\Session\Middleware\AuthenticateSession::class,
-		\Illuminate\Routing\Middleware\SubstituteBindings::class,
-		\Illuminate\Auth\Middleware\Authorize::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\Authenticate::class,
+        \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        \Illuminate\Session\Middleware\AuthenticateSession::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        \Illuminate\Auth\Middleware\Authorize::class,
 	];
 }
